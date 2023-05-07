493256c25d81595029e1362c0c65612ba46211f4
gurpreetengineer <gurpreet.reactdev@gmail.com>
2023-05-07 15:58:58 +0530

    GAME - added jigsaw puzzle in python

---
+++ python_snippets/games/jigsaw_puzzle.py
@@ -0,0 +1,112 @@
+import pygame
+import random
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Set up the display
+pygame.init()
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Jigsaw Puzzle")
+
+# Load the image
+image = pygame.image.load("image.jpg")
+image_width, image_height = image.get_rect().size
+
+# Break the image into puzzle pieces
+rows = 4
+cols = 4
+piece_width = image_width // cols
+piece_height = image_height // rows
+pieces = []
+for i in range(rows):
+  for j in range(cols):
+    x = j * piece_width
+    y = i * piece_height
+    rect = pygame.Rect(x, y, piece_width, piece_height)
+    image_piece = pygame.Surface(rect.size).convert()
+    image_piece.blit(image, (0, 0), rect)
+    pieces.append(image_piece)
+random.shuffle(pieces)
+
+# Set up the game variables
+selected_piece = None
+dragging = False
+solved = False
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)
+timer_event = pygame.USEREVENT + 1
+pygame.time.set_timer(timer_event, 1000)
+time_remaining = 60
+hint_count = 3
+
+# Game loop
+done = False
+while not done:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done = True
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      if not dragging:
+        for i, piece in enumerate(pieces):
+          if piece.get_rect().collidepoint(event.pos):
+            selected_piece = i
+            dragging = True
+            break
+    elif event.type == pygame.MOUSEBUTTONUP:
+      if dragging:
+        dragging = False
+        for i, piece in enumerate(pieces):
+          if i != selected_piece:
+            if piece.get_rect().colliderect(pieces[selected_piece].get_rect()):
+              pieces[i], pieces[selected_piece] = pieces[selected_piece], pieces[i]
+              break
+        selected_piece = None
+    elif event.type == timer_event:
+        time_remaining -= 1
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_h and hint_count > 0:
+        hint_count -= 1
+        for i, piece in enumerate(pieces):
+          if piece.get_rect().collidepoint(pygame.mouse.get_pos()):
+            piece.set_alpha(100)
+            pygame.time.set_timer(pygame.USEREVENT + 2, 1000)
+            break
+      elif event.key == pygame.K_r:
+        time_remaining = 60
+        hint_count = 3
+        solved = False
+        random.shuffle(pieces)
+
+  # Draw the puzzle pieces
+  screen.fill(WHITE)
+  for i, piece in enumerate(pieces):
+    row = i // cols
+    col = i % cols
+    x = col * piece_width
+    y = row * piece_height
+    screen.blit(piece, (x, y))
+    if solved:
+      pygame.draw.rect(screen, (0, 255, 0), (x, y, piece_width, piece_height), 5)
+
+# Draw the timer and hint count
+timer_text = font.render("Time: " + str(time_remaining), True, BLACK)
+hint_text = font.render("Hints: " + str(hint_count), True, BLACK)
+screen.blit(timer_text, (10, 10))
+screen.blit(hint_text, (10, 50))
+
+# Check if the puzzle is solved
+if not solved:
+  solved = all(pieces[i] == image.subsurface((i%cols)*piece_width, (i//cols)*piece_height, piece_width, piece_height) for i in range(len(pieces)))
+  if solved:
+    pygame.time.set_timer(timer_event, 0)
+    solved_text = font.render("Congratulations, you solved the puzzle!", True, BLACK)
+    screen.blit(solved_text, (size[0]//2 - solved_text.get_width()//2, size[1]//2 - solved_text.get_height()//2))
+
+# Update the display
+pygame.display.flip()
+clock.tick(60)
+
+pygame.quit()
