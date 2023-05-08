a6a479a3f3e093d25bd713fc9350c652c54ccc46
gurpreetengineer <gurpreet.reactdev@gmail.com>
2023-05-08 23:49:02 +0530

    Bubble sort algorithm in c++ - added explanatory comments

--- CPP_language_snippet/algorithms/bubble_sort.cpp
+++ CPP_language_snippet/algorithms/bubble_sort.cpp
@@ -48,3 +48,18 @@ int main()
 
   return 0;
 }
+
+
+/**
+ * @brief explanation of bubble sort function
+ * This program defines a bubbleSort function that takes an integer array and its size as input and sorts the array in ascending order using the bubble sort algorithm.
+
+ * The bubble sort algorithm works by repeatedly swapping adjacent elements in the array if they are in the wrong order, until the entire array is sorted. 
+ * In the bubbleSort function, we use two nested loops to iterate over all pairs of adjacent elements in the array. 
+ * If a pair of adjacent elements is in the wrong order, we swap them using the swap function from the <algorithm> header. 
+ * The outer loop iterates n-1 times because the largest element will have already bubbled up to the end of the array after n-1 iterations. 
+ * The inner loop iterates n-i-1 times because the last i elements of the array will already be sorted after i iterations of the outer loop.
+ * 
+ * In the main function, we define an integer array arr and its size n, and call the bubbleSort function to sort the array. We then print the original and sorted arrays using a for loop.
+ * 
+ */
